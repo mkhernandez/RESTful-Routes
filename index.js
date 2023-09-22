6b828491d313dc7e551ff6ba835113ff274be4b3
Mark Hernandez <markkimhernandez@gmail.com>
2023-09-22 18:00:12 -0500

    Added index route for all comments

--- index.js
+++ index.js
@@ -2,11 +2,29 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const log = console.log;
-
 const port = 5000;
 const localHost = 'localhost';
+const comments = [
+    {
+        username: 'Todd',
+        comment: 'lol that is so funny!'
+    },
+    {
+        username: 'Skyler',
+        comment: 'I like to go birdwatching with my dog'
+    },
+    {
+        username: 'Sk8erBoi',
+        comment: 'Plz delete your account, Todd'
+    },
+    {
+        username: 'onlysayswoof',
+        comment: 'woof woof woof'
+    }
+]
 
 // set view engine
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // body parser. The body of the incoming payload of a post request must be parsed.
@@ -16,8 +34,9 @@ app.use(express.urlencoded({extended: true}));
 // body parser for json. Must use this if incoming request is json data
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('We are at the home page');
+// Index route. Example of getting all comments.
+app.get('/comments', (req, res) => {
+    res.render('comments/index', {comments});
 });
 
 app.get('/tacos', (req, res) => {
